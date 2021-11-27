0ac9e8a771c64612ea7b47882cff3d5b7efdf8fb
Joel Bergstein <joel@thetrackavl.com>
2021-11-27 18:59:15 -0500

    another fix?

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 Vue.createApp({
     mounted: function(){
-        this.interval = setInterval(() => this.updateDediInfo(), 2000);
+        // document.documentElement.setAttribute('data-theme', targetTheme);
+        // localStorage.setItem('theme', targetTheme);
+
+        this.interval = setInterval(() => this.updateDediInfo(), 1000);
     },
     data(){
         return {
             header: "Dedi it Up!",
             timer: 0,
             dedis:[
-                {dediId:'1',serverUp:true,dediPort:'51298', serverName: 'Custom 1', modName: 'Petit Petit', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'2',serverUp:false,dediPort:'52298', serverName: 'Custom 2', modName: 'Kansas - Xfinitys', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'3',serverUp:true,dediPort:'53298', serverName: 'Custom 3', modName: 'Bridge - 73 RSR', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'4',serverUp:false,dediPort:'54298', serverName: 'Custom 4', modName: 'Imola - 134 Judd', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'5',serverUp:false,dediPort:'54351', serverName: 'Enduro', modName: 'Petit Petit', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'6',serverUp:false,dediPort:'51301', serverName: 'Drop-In', modName: 'Suzuka - F3', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'7',serverUp:false,dediPort:'31298', serverName: 'Sched A1', modName: 'Watkins - Fiat 128', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'8',serverUp:false,dediPort:'32298', serverName: 'Sched A2', modName: 'Watkins - USF-17', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'9',serverUp:true,dediPort:'33298', serverName: 'Sched B1', modName: 'Lime Rock - Fiat 128', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'10',serverUp:false,dediPort:'34298', serverName: 'Sched B2', modName: 'Lime Rock - USF-17', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'11',serverUp:false,dediPort:'35298', serverName: 'Sched C1', modName: '---', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'12',serverUp:false,dediPort:'36298', serverName: 'Sched C2', modName: '---', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'13',serverUp:false,dediPort:'53301', serverName: 'Mixed League', modName: 'Road America', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'14',serverUp:false,dediPort:'54301', serverName: 'LMP3 League', modName: 'Snetterton', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'},
-                {dediId:'15',serverUp:false,dediPort:'52301', serverName: 'Rookie League', modName: 'Portland - Alpine', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5'}
+                {dediId:'1',serverUp:'0',apiFail:'0',dediPort:'51298',serverName: 'Custom 1',modName: 'Petit Petit',serverSession: 'Practice',serverSessionTimeLeft: '2:33:27',serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'2',serverUp:'0',apiFail:'0',dediPort:'52298', serverName: 'Custom 2', modName: 'Kansas - Xfinitys', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'3',serverUp:'0',apiFail:'0',dediPort:'53298', serverName: 'Custom 3', modName: 'Bridge - 73 RSR', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'4',serverUp:'0',apiFail:'0',dediPort:'54298', serverName: 'Custom 4', modName: 'Imola - 134 Judd', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'5',serverUp:'0',apiFail:'0',dediPort:'54351', serverName: 'Enduro', modName: 'Petit Petit', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'6',serverUp:'0',apiFail:'0',dediPort:'51301', serverName: 'Drop-In', modName: 'Suzuka - F3', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'7',serverUp:'0',apiFail:'0',dediPort:'31298', serverName: 'Sched A1', modName: 'Watkins - Fiat 128', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'8',serverUp:'0',apiFail:'0',dediPort:'32298', serverName: 'Sched A2', modName: 'Watkins - USF-17', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'9',serverUp:'0',apiFail:'0',dediPort:'33298', serverName: 'Sched B1', modName: 'Lime Rock - Fiat 128', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'10',serverUp:'0',apiFail:'0',dediPort:'34298', serverName: 'Sched B2', modName: 'Lime Rock - USF-17', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'11',serverUp:'0',apiFail:'0',dediPort:'35298', serverName: 'Sched C1', modName: '---', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'12',serverUp:'0',apiFail:'0',dediPort:'36298', serverName: 'Sched C2', modName: '---', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'13',serverUp:'0',apiFail:'0',dediPort:'53301', serverName: 'Mixed League', modName: 'Road America', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'14',serverUp:'0',apiFail:'0',dediPort:'54301', serverName: 'LMP3 League', modName: 'Snetterton', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]},
+                {dediId:'15',serverUp:'0',apiFail:'0',dediPort:'52301', serverName: 'Rookie League', modName: 'Portland - Alpine', serverSession: 'Practice', serverSessionTimeLeft: '2:33:27', serverNumberDrivers: '5',
+                    drivers:[]}
             ]
         }
     },
@@ -32,30 +50,57 @@ Vue.createApp({
                 var proxy_ip = '192.168.1.17';
                 var dedi_port = dedi.dediPort;
                 var proxy_port = '3000';
-                var url = 'http://' + proxy_ip + ':' + proxy_port + '/?port=' + dedi_port;
-                axios.get(url)
+                var url = 'http://' + proxy_ip + ':' + proxy_port + '/session/?port=' + dedi_port;
+                axios.get(url,{timeout: 500})
                     .then(function (response) {
-                        // handle success
+                        // handle success - reset our failure counter
+                        dedis[index].apiFail = 0;
                         // console.log(response);
                         rawData = response.data;
                         // console.log(rawData);
-                        console.log(dedis[index]);
+                        // console.log(dedis[index]);
                         dedis[index].serverName = rawData.playerFileName;
                         dedis[index].serverSession = rawData.session;
                         dedis[index].serverSessionTimeLeft = new Date((rawData.endEventTime - rawData.currentEventTime) * 1000).toISOString().substr(11, 8);
                         dedis[index].serverNumberDrivers = rawData.numberOfVehicles;
+                        dedis[index].serverUp = rawData.numberOfVehicles;
                         dedis[index].modName = rawData.serverName;
-                        dedis[index].serverUp = true;
-                        console.log(dedis[index]);
+                        // console.log(dedis[index]);
+                        //now grab driver info
+                        var driver_url = 'http://' + proxy_ip + ':' + proxy_port + '/standings/?port=' + dedi_port;
+                        axios.get(driver_url,{timeout: 500})
+                            .then(function (response) {
+                                driver_data = response.data;
+                                drivers = [];
+                                response.data.forEach(function (driver_raw,driver_index,drivers_raw) {
+                                    var driver = {};
+                                    driver.driverName = driver_raw.driverName;
+                                    driver.driverPosition = driver_raw.position;
+                                    driver.driverPenalty = driver_raw.penalties > 0 ? true : false;
+                                    driver.driverLaps = driver_raw.lapsCompleted;
+                                    driver.driverOnTrack = driver_raw.inGarageStall ? false : true;
+                                    driver.driverInPit = driver_raw.pitting;
+                                    drivers.push(driver);
+                                });
+                                dedis[index].drivers = drivers;
+                            })
                     })
                     .catch(function (error) {
-                        // handle error
-                        // dedis[index].serverUp = false;
+                        // handle error - build in a debounce for the odd failure
+                        dedis[index].apiFail = dedis[index].apiFail + 1;
+                        if (dedis[index].apiFail > 5) {
+                            dedis[index].serverUp = '-1';
+                        }
                         console.log(error);
                     })
             });
         }
-    }
+    },
+    computed: {
+        orderedServers: function () {
+        return _.orderBy(this.dedis, 'serverUp','desc')
+        }
+      }
 })
 .component('dedi-card',{
     template: '#dedi-card-template',
@@ -65,7 +110,9 @@ Vue.createApp({
         'server_session',
         'server_session_time_left',
         'server_number_drivers',
-        'server_up'
+        'server_up',
+        'drivers',
+        'dedi_port'
     ],
     data(){
         return {
@@ -80,10 +127,13 @@ Vue.createApp({
         'server_session',
         'server_session_time_left',
         'server_number_drivers',
-        'server_up'
+        'server_up',
+        'drivers',
+        'dedi_port'
     ],
     data(){
         return {
+            selected_drivers: []
         }
     }
 })
@@ -112,5 +162,30 @@ Vue.createApp({
     props:[
         'server_name'
     ]
+})
+.component('clear-penalty-button',{
+    template: '#clear-penalty-button-template',
+    props: [
+        'selected_drivers',
+        'dedi_port'
+    ],
+    methods: {
+        clearPenalty: function(selected_drivers,dedi_port) {
+            var proxy_ip = '192.168.1.17';
+            var proxy_port = '3000';
+            selected_drivers.forEach( function(driver,index,selected_drivers) {
+                var url = 'http://' + proxy_ip + ':' + proxy_port + '/clear-penalty/?port=' + dedi_port + '&driver=' + encodeURIComponent(driver.id);
+                console.log(url);
+                axios.post(url,{timeout: 500})
+                .then(function (response) {
+                    console.log()
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+            })
+        }
+    }
 })
 .mount('#server-list')
