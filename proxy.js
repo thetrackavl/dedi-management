d2407ac84d9f410b0a5a1c75fb09c237a6334821
Joel Bergstein <joel@thetrackavl.com>
2021-11-27 10:06:37 -0500

    added standings method

--- proxy.js
+++ proxy.js
@@ -8,13 +8,26 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
+app.get('/session', (req, res) => {
     let port = req.query.port;
     request(
         { url: 'http://localhost:' + port + '/rest/watch/sessionInfo' },
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
-                return res.status(500).json({ type: 'error', message: err.message });
+                return res.status(500).json({ type: 'error', message: err });
+            }
+            res.json(JSON.parse(body));
+        }
+    )
+});
+
+app.get('/standings', (req, res) => {
+    let port = req.query.port;
+    request(
+        { url: 'http://localhost:' + port + '/rest/watch/standings' },
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: err });
             }
             res.json(JSON.parse(body));
         }
